e54e3efbe27ef8a5bd51ac4e7cd7dc21a6baad14
BilalAM <bilal.asif.97@gmail.com>
2017-09-22 18:38:18 +0500

    deleted nature dao

--- src/com/data/DAO/NatureMongoConcrete.java
+++
@@ -1,9 +0,0 @@
-package com.data.DAO;
-
-import com.data.MODELS.Nature;
-
-public class NatureMongoConcrete extends MongoConcrete<Nature> {
-	public NatureMongoConcrete() {
-		super("NatureCollection");
-	}
-}
--- src/com/data/MODELS/Nature.java
+++
@@ -1,5 +0,0 @@
-package com.data.MODELS;
-
-public class Nature {
-
-}
