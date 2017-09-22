00976fe992e97ae7269cdb83dd3edd12a5bed5d1
BilalAM <bilal.asif.97@gmail.com>
2017-09-22 18:41:07 +0500

    added properties

--- src/com/data/MODELS/Vehicle.java
+++ src/com/data/MODELS/Vehicle.java
@@ -1,5 +1,43 @@
 package com.data.MODELS;
 
-public class Vehicle {
+import java.io.Serializable;
+
+public class Vehicle implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7953839798341810420L;
+	
+	
+	private int vehicleCode;
+	private String vehMake;
+	private String vehColour;
+	private double vehPrice;
+	
+	public int getVehicleCode() {
+		return vehicleCode;
+	}
+	public void setVehicleCode(int vehicleCode) {
+		this.vehicleCode = vehicleCode;
+	}
+	public String getVehMake() {
+		return vehMake;
+	}
+	public void setVehMake(String vehMake) {
+		this.vehMake = vehMake;
+	}
+	public String getVehColour() {
+		return vehColour;
+	}
+	public void setVehColour(String vehColour) {
+		this.vehColour = vehColour;
+	}
+	public double getVehPrice() {
+		return vehPrice;
+	}
+	public void setVehPrice(double vehPrice) {
+		this.vehPrice = vehPrice;
+	}
 
 }
