f24b09cc7a371e0074c923801624650e4d84586a
BilalAM <bilal.asif.97@gmail.com>
2017-09-28 21:17:10 +0500

    mappy class

--- src/com/data/mapper/Mappy.java
+++ src/com/data/mapper/Mappy.java
@@ -3,16 +3,47 @@
  */
 package com.data.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.json.JSONObject;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
 /**
  * @author bilalam
  *
  */
-public class Mappy {
+public class Mappy implements IMap{
+
+	private static final MongoClient MONGO_CLIENT = new MongoClient("localhost:27017");
+	private static final MongoDatabase MONGO_DATABASE = MONGO_CLIENT.getDatabase("ProductDB");
+	private static MongoCollection<Document> collection;
+	private List<String> attributeList = new ArrayList<String>();
 
 	
+	@Override
+	public List<String> getAttributes(String collectionName) {
+		collection = MONGO_DATABASE.getCollection(collectionName);
+		JSONObject object = new JSONObject(collection.find(Filters.exists("_id")).first().toJson());
+		String[] fieldNames = JSONObject.getNames(object);
+		for (String s : fieldNames) {
+			attributeList.add(s);
+		}
+		return attributeList;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Mappy mappy = new Mappy();
+		
+		for(String s : mappy.getAttributes("orderCollection")){
+			System.out.println(s);
+		}
 	}
 
 }
