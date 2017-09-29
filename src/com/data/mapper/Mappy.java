c9af5445baa8c81fa54f113e61d0c3311e0a5491
BilalAM <bilal.asif.97@gmail.com>
2017-09-29 14:11:35 +0500

    changes

--- src/com/data/mapper/Mappy.java
+++ src/com/data/mapper/Mappy.java
@@ -23,11 +23,12 @@ public class Mappy implements IMap{
 	private static final MongoClient MONGO_CLIENT = new MongoClient("localhost:27017");
 	private static final MongoDatabase MONGO_DATABASE = MONGO_CLIENT.getDatabase("ProductDB");
 	private static MongoCollection<Document> collection;
-	private List<String> attributeList = new ArrayList<String>();
+	private List<String> attributeList;
 
 	
 	@Override
 	public List<String> getAttributes(String collectionName) {
+		attributeList = new ArrayList<>();
 		collection = MONGO_DATABASE.getCollection(collectionName);
 		JSONObject object = new JSONObject(collection.find(Filters.exists("_id")).first().toJson());
 		String[] fieldNames = JSONObject.getNames(object);
@@ -44,6 +45,13 @@ public class Mappy implements IMap{
 		for(String s : mappy.getAttributes("orderCollection")){
 			System.out.println(s);
 		}
+		
+		System.out.println("\n");
+		
+		for(String s : mappy.getAttributes("ProductCollection")){
+			System.out.println(s);
+		}
+		
 	}
 
 }
