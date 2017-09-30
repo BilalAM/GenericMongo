1f5468f0130b4e115a2ea804f9cafd617fa73a79
BilalAM <bilal.asif.97@gmail.com>
2017-09-30 12:29:18 +0500

    ModelMaker added

--- src/com/data/mapper/ModelMaker.java
+++ src/com/data/mapper/ModelMaker.java
@@ -1,7 +1,19 @@
 package com.data.mapper;
+import java.util.List;
+
 import com.data.mapper.Mappy;
 
 
 public class ModelMaker extends Mappy{
+	
+	@Override
+	public List<String> getAttributes(String collectionName) {
+		// TODO Auto-generated method stub
+		return super.getAttributes(collectionName);
+	}
+	
+	
+	
+	
 	
 }
