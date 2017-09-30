03a8a056ec2672e019b0cb4b8dd1a16810c08ddb
BilalAM <bilal.asif.97@gmail.com>
2017-09-30 12:27:06 +0500

    changed to abstract

--- src/com/data/mapper/Mappy.java
+++ src/com/data/mapper/Mappy.java
@@ -18,40 +18,22 @@ import com.mongodb.client.model.Filters;
  * @author bilalam
  *
  */
-public class Mappy implements IMap{
+public abstract class Mappy implements IMap {
 
 	private static final MongoClient MONGO_CLIENT = new MongoClient("localhost:27017");
-	private static final MongoDatabase MONGO_DATABASE = MONGO_CLIENT.getDatabase("ProductDB");
+	private static final MongoDatabase MONGO_DATABASE = MONGO_CLIENT.getDatabase("ComplaintsDataset");
 	private static MongoCollection<Document> collection;
 	private List<String> attributeList;
 
-	
 	@Override
 	public List<String> getAttributes(String collectionName) {
 		attributeList = new ArrayList<>();
 		collection = MONGO_DATABASE.getCollection(collectionName);
-		JSONObject object = new JSONObject(collection.find(Filters.exists("_id")).first().toJson());
+		JSONObject object = new JSONObject(collection.find(Filters.exists("_id")).skip(1).first().toJson());
 		String[] fieldNames = JSONObject.getNames(object);
 		for (String s : fieldNames) {
 			attributeList.add(s);
 		}
 		return attributeList;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Mappy mappy = new Mappy();
-		
-		for(String s : mappy.getAttributes("orderCollection")){
-			System.out.println(s);
-		}
-		
-		System.out.println("\n");
-		
-		for(String s : mappy.getAttributes("ProductCollection")){
-			System.out.println(s);
-		}
-		
-	}
-
 }
