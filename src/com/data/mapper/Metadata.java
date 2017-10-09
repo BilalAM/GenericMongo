96cde41f6ebef4de98b40dbafbccf6055cad7156
BilalAM <bilal.asif.97@gmail.com>
2017-10-09 13:53:15 +0500

    metadata class added , have to revise

---
+++ src/com/data/mapper/Metadata.java
@@ -0,0 +1,66 @@
+package com.data.mapper;
+
+import java.util.HashMap;
+import java.util.*;
+
+public class Metadata {
+	private String dbName;
+	private String collectionName;
+	private String collectionClass;
+	private String attirbuteName;
+	private String attributeKey;
+	private String attributeType;
+
+	public String getDbName() {
+		return dbName;
+	}
+
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+
+	public String getCollectionName() {
+		return collectionName;
+	}
+
+	public void setCollectionName(String collectionName) {
+		this.collectionName = collectionName;
+	}
+
+	public String getCollectionClass() {
+		return collectionClass;
+	}
+
+	public void setCollectionClass(String collectionClass) {
+		this.collectionClass = collectionClass;
+	}
+
+	public String getAttributeKey() {
+		return attributeKey;
+	}
+
+	public void setAttributeKey(String attributeKey) {
+		this.attributeKey = attributeKey;
+	}
+
+	public String getAttributeType() {
+		return attributeType;
+	}
+
+	public void setAttributeType(String attributeType) {
+		this.attributeType = attributeType;
+	}
+
+	public String getAttirbuteName() {
+		return attirbuteName;
+	}
+
+	public void setAttirbuteName(String attirbuteName) {
+		this.attirbuteName = attirbuteName;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
