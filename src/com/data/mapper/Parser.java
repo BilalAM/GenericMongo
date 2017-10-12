3f9194ca7910071d46a41890a3450c51fac6a6cf
BilalAM <bilal.asif.97@gmail.com>
2017-10-12 23:24:15 +0500

    some changes

--- src/com/data/mapper/Parser.java
+++ src/com/data/mapper/Parser.java
@@ -17,6 +17,10 @@ public class Parser {
 	private static org.w3c.dom.Document document;
 	private static List<String> collectionAttributesData = new ArrayList<>();
 
+	static {
+		initialize();
+	}
+
 	private static void initialize() {
 
 		try {
@@ -32,20 +36,17 @@ public class Parser {
 	public Metadata parse() {
 		Metadata data = new Metadata();
 		List<CollectionMetaData> collections = new ArrayList<>();
-		List<Node> attributeNodes = new ArrayList<>();
+		List<Node> attributeNodes = null;
 		CollectionMetaData collection = null;
 
 		NodeList list = document.getDocumentElement().getChildNodes();
 		for (int i = 0; i < list.getLength(); i++) {
-			// System.out.println(list.getLength());
 			Node tempNode = list.item(i);
 			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
-				System.out.println(tempNode.getNodeName());
 				/* setting dbName */
 				data.setDbName(tempNode.getAttributes().getNamedItem("name").getTextContent());
 				// get collection nodes against dbName node
 				NodeList childsOfDb = tempNode.getChildNodes();
-				// System.out.println(childsOfDb.getLength());
 				// traverse the collection nodes
 				for (int j = 0; j < childsOfDb.getLength(); j++) {
 					// get a single collection node
@@ -53,16 +54,12 @@ public class Parser {
 					// check it if it is really a element node
 					if (tempChildOfDbNode.getNodeType() == Node.ELEMENT_NODE) {
 						// initialize the metadata for a SINGLE collection
-						/**/ collection = new CollectionMetaData();
-
-						System.out.println("  " + tempChildOfDbNode.getNodeName());
+						collection = new CollectionMetaData();
+						attributeNodes = new ArrayList<>();	
 
 						// get the attributes of the collection node
 						NamedNodeMap attributesOfCollection = tempChildOfDbNode.getAttributes();
-						// print them for testing
-						System.out.println(tempChildOfDbNode.getNodeName() + "atr --> "
-								+ attributesOfCollection.getNamedItem("name") + "   "
-								+ attributesOfCollection.getNamedItem("class"));
+						
 						// set the collection name and collection class name attributes
 						collection.setCollectionName(
 								tempChildOfDbNode.getAttributes().getNamedItem("name").getTextContent());
@@ -79,7 +76,6 @@ public class Parser {
 							if (tempChildOfCollection.getNodeType() == Node.ELEMENT_NODE) {
 								// add the attribute node completely into the attributeNodes list
 								attributeNodes.add(tempChildOfCollection);
-								System.out.println("     " + tempChildOfCollection.getNodeName());
 							}
 						}
 						// after the list is filled , set the attribute nodes to the collectionmetadata
@@ -89,11 +85,11 @@ public class Parser {
 					}
 
 				}
-				System.out.println("size of collections list --> " + collections.size());
+				// System.out.println("size of collections list --> " + collections.size());
 				data.setCollections(collections);
 			}
 		}
-		System.out.println("ds");
+		// System.out.println("ds");
 		return data;
 	}
 
