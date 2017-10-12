e6d9356d856da662e2fc039ae1c33e617b2d6816
BilalAM <bilal.asif.97@gmail.com>
2017-10-12 21:21:05 +0500

    major changes made to MetaData , see comment

---
+++ src/com/data/mapper/CollectionMetaData.java
@@ -0,0 +1,33 @@
+package com.data.mapper;
+
+import java.util.*;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class CollectionMetaData {
+
+	private List<Node> attributeNodes;
+	private String collectionName;
+	private String collectionClassName;
+	
+	public List<Node> getAttributeNodes() {
+		return attributeNodes;
+	}
+	public void setAttributeNodes(List<Node> attributeNodes) {
+		this.attributeNodes = attributeNodes;
+	}
+	public String getCollectionName() {
+		return collectionName;
+	}
+	public void setCollectionName(String collectionName) {
+		this.collectionName = collectionName;
+	}
+	public String getCollectionClassName() {
+		return collectionClassName;
+	}
+	public void setCollectionClassName(String collectionClassName) {
+		this.collectionClassName = collectionClassName;
+	}
+
+}
