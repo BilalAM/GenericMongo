256355cf0fc07f957521b39bfeb86256c624a618
BilalAM <bilal.asif.97@gmail.com>
2017-10-12 21:48:11 +0500

    methods

--- src/com/data/mapper/Parser.java
+++ src/com/data/mapper/Parser.java
@@ -1,8 +1,10 @@
 package com.data.mapper;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Parser {
@@ -10,4 +12,17 @@ public class Parser {
 	private static final DocumentBuilderFactory parserFactory = DocumentBuilderFactory.newInstance();
 	private static org.w3c.dom.Document document;
 	private static List<String> collectionAttributesData = new ArrayList<>();
+	
+	private static void initialize() {
+
+		try {
+			File file = new File(MAPPING_FILE);
+			DocumentBuilder builder = parserFactory.newDocumentBuilder();
+			document = builder.parse(file);
+			document.getDocumentElement().normalize();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
